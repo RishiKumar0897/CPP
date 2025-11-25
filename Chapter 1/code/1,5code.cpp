f1e3092419874a9ef6f0dfc20f298d159a9d6c71
Rishi Kumar <rishikumar0897@gmail.com>
2025-11-24 22:40:02 -0500

    now on Chapter 1.7

---
+++ Chapter 1/code/1,5code.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+int main() {
+    //std::cout << "Hello, World!"; //prints Hello, World! to the console
+
+    //std::cout << 42; // prints the number 42 to the console
+
+    // int x{5};
+    // std::cout << "The value of x is: " << x << std::endl; // prints the value of x to the console
+
+
+    //std::cout << "Hello" << "World"; // prints HelloWorld to the console
+
+
+
+
+
+    // std::cout << "Hi!" << std::endl;
+    // std::cout << "My name is Rishi." << std::endl; // prints My name is Rishi. to the console
+
+
+    
+    // this is more efficient than using std::endl
+    // int y {5};
+    // std::cout << "y is equal to " << y << '\n'; // prints y is equal to 5 to the console
+
+
+    int x{};
+    std::cout << "Enter a number: ";
+    std::cin >> x;
+    std::cout << "You entered: " << x << '\n';
+
+
+    
+
+
+
+    return 0;
+}
+
